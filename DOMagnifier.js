3710c8831947a68c79261976a982b33ff5bcc4c7
shaylh <lachmanovich@gmail.com>
2014-08-26 22:25:30 +0200

    Added documentation to the md file, WIP

--- DOMagnifier.js
+++ DOMagnifier.js
@@ -1,20 +1,26 @@
-var Magnifier = function (targetId) {
-
-    this.paintCroppedImageBound = this.paintCroppedImage.bind(this);
-    this.startBound = this.start.bind(this);
-
-    this.box = document.getElementById(targetId);
-    this.result = document.createElement('div');
-    this.result.className = 'magnifier';
-    this.croppedCanvas = document.createElement('canvas');
-    this.result.appendChild(this.croppedCanvas);
-    document.body.appendChild(this.result);
+var Magnifier = function (targetId, zoomLevel) {
+    this.zoomLevel = zoomLevel || 2;
+    this.setupDOM(targetId);
+    this.bindFunctions();
 };
 
 Magnifier.prototype = {
+    bindFunctions: function(){
+        this.paintCroppedImageBound = this.paintCroppedImage.bind(this);
+        this.startBound = this.start.bind(this);
+    },
+    setupDOM: function(targetId){
+        this.box = document.getElementById(targetId);
+        this.result = document.createElement('div');
+        this.result.className = 'magnifier';
+        this.croppedCanvas = document.createElement('canvas');
+        this.result.appendChild(this.croppedCanvas);
+        document.body.appendChild(this.result);
+    },
     start: function () {
         html2canvas(this.box, {onrendered: this.onGotCanvas.bind(this)});
         this.resetOffsetValues();
+        return this;
     },
     stop: function(){
         this.unregisterEvents();
@@ -36,6 +42,7 @@ Magnifier.prototype = {
     registerEvents: function(){
         this.box.addEventListener('mousemove', this.paintCroppedImageBound);
         this.result.addEventListener('mousemove', this.paintCroppedImageBound);
+        this.result.addEventListener('dblclick', this.stop.bind(this));
         window.addEventListener('resize', this.startBound);
     },
     unregisterEvents: function(){
@@ -63,7 +70,7 @@ Magnifier.prototype = {
         var y2 = Math.min(canvas.height, Math.max(1, bounds.top + bounds.height));
         var width = this.croppedCanvas.width = x2 - x1;
         var height = this.croppedCanvas.height = y2 - y1;
-        this.croppedCanvas.getContext("2d").scale(2, 2);
+        this.croppedCanvas.getContext("2d").scale(this.zoomLevel, this.zoomLevel);
         this.croppedCanvas.getContext("2d").drawImage(canvas, x1, y1, width, height, 0, 0, width, height);
     }
 };
--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 DOMagnifier
 =========
 
-Add a nice looking magnifier on any DOM element you like
+# Setup
+To enable DOMagnifier in a page, add the following to the header section:
+
+```
+<script src="DOMagnifier.js"></script>
+```
+```
+<script src="html2canvas.min.js"></script>
+```
+```
+<link href="css/magnifier.css" rel="stylesheet"/>
+```
+# Usage
+
+## Starting
+To add the actual magnifier, create a new magnifier object, and call start().
+
+```javascript
+new Magnifier('someElementId').start();
+```
+
+Where `someElementId` is the id of the DOM element you want to enable magnifier for.
+Once this is done, you'll see a nice looking magnifier over your DOM element.
+
+## Stopping
+To remove the magnifier, simply double click the mouse, or call stop() on the magnifier object.
+```javascript
+var magnifier = new Magnifier('someElementId').start();
+magnifier.stop();
+```
+
+## Additional parameters
+### Zoom level
+by default, the zoom level is x2; However, you can pass the magnifier's constructor a second parameter, indicating the zoom level:
+```javascript
+new Magnifier('someElementId', 3);
+```
